750c99f0f457ec082fdbaaecf6ae5a49d79fdaf5
Daniel Medina <paginamedina@gmail.com>
2023-07-05 10:50:21 -0500

    next-flask

---
+++ client/pages/index.tsx
@@ -0,0 +1,19 @@
+import React, {useEffect,useState} from 'react'
+
+
+//rfce
+
+
+function index() {
+/*
+  useEffect(() => {
+    fetch("http://127.0.0.1:8080/api/home")
+  }, [])
+  */
+
+  return (
+    <div>index</div>
+  )
+}
+
+export default index
